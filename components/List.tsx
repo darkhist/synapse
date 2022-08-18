a6888cc28a618d2b1e5bcf07c523ed083e99719b
Quinn Salas <quinn@openloophealth.com>
2022-08-18 14:24:35 -0500

    fix: improve timestamps

--- components/List.tsx
+++ components/List.tsx
@@ -11,7 +11,9 @@ const List = ({ thoughts }: Props) => (
             <Link key={slug} href={`/thoughts/${slug}`}>
               <li className="flex flex-col md:px-10 py-5 cursor-pointer hover:bg-[#ffffff0f] hover:translate-x-1">
                 <div className="text-l text-gray-400 mx-5">
-                  {new Date(date).toDateString()}
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "full",
+                  }).format(new Date(date))}
                 </div>
                 <div className="px-5 py-2">
                   <a className="text-xl md:text-2xl text-left font-bold underline">
