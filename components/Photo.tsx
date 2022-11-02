c74187acc24030861b58ede961d20694d7587924
Quinn Salas <quinn@openloophealth.com>
2022-11-02 00:43:56 -0500

    fix: prefer next/legacy/image for now

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 interface Props {
   src: string;
@@ -10,8 +10,8 @@ const Photo = ({ src, alt }: Props) => (
     <Image
       src={src}
       alt={alt}
-      width="100%"
-      height="100%"
+      width={100}
+      height={100}
       layout="responsive"
       objectFit="contain"
     />
